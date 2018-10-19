4e57f446c1d1a85a085e057ac865ce905d517b43
khantwaikyaw <khantwaikyaw11@gmail.com>
2018-10-19 12:24:01 +0630

    netlify cms added

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import Admin from '../pages/admin/Admin';
 import ComingSoon from '../pages/comingsoon/ComingSoon';
 import NotFound from '../pages/errors/NotFound';
 
 export default () => (
   <Switch>
     <Route exact path="/" component={ComingSoon} />
+    <Route exact path="/admin" component={Admin} />
     <Route patah="*" component={NotFound} />
   </Switch>
 );
---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import CMS from 'netlify-cms';
+
+const Admin = () => <div />;
+
+export default Admin;
